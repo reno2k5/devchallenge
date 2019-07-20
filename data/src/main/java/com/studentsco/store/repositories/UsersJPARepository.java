c76965cc7ac34bdb7182d2ab7dcfb4d72eab5b08
Oscar <reno2k5@gmail.com>
2019-07-20 13:08:00 -0600

    Revert "Forward changes"
    This reverts commit 55e1269d140e9db3ac0f8240979b434f34fe6fcc.

--- data/src/main/java/com/studentsco/store/repositories/UsersJPARepository.java
+++
@@ -1,9 +0,0 @@
-package com.studentsco.store.repositories;
-
-import com.studentsco.store.model.security.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UsersJPARepository extends CrudRepository<User, Integer>{
-    
-    public User findByUsername();
-}
--- restapp/src/main/java/com/studentsco/store/api/ErrorController.java
+++
@@ -1,8 +0,0 @@
-package com.studentsco.store.api;
-
-import org.springframework.web.bind.annotation.RestController;
-
-
-public enum ErrorController {
-    
-}
