685ea819f2dfd8bca9fc1d613aadff949dccfc26
Oscar Rivera <reno2k5@gmail.com>
2019-07-21 12:23:42 -0600

    Spring security over DB configuration

--- restapp/src/main/java/com/studentsco/store/security/SpringSecurityConfig.java
+++ restapp/src/main/java/com/studentsco/store/security/SpringSecurityConfig.java
@@ -1,38 +1,39 @@
 package com.studentsco.store.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 @EnableAutoConfiguration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic().and()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/product/**").permitAll()
-                .antMatchers(HttpMethod.POST,"/product/*/like").fullyAuthenticated()
-                .antMatchers(HttpMethod.PUT, "/product/**").hasRole("ADMIN")
-                .antMatchers(HttpMethod.POST, "/product/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/product/*/like").fullyAuthenticated()
+                .antMatchers(HttpMethod.PUT, "/product/**").hasAuthority("ADMIN")
+                .antMatchers(HttpMethod.POST, "/product/**").hasAuthority("ADMIN")
                 .antMatchers(HttpMethod.POST, "/buy/**").fullyAuthenticated()
                 .and()
                 .csrf().disable()
                 .formLogin().disable();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        auth.inMemoryAuthentication()
-                .withUser("user").password("{noop}password").roles("USER")
-                .and()
-                .withUser("admin").password("{noop}password").roles("USER", "ADMIN");
-
+    @Bean
+    public NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
     }
 
 }
