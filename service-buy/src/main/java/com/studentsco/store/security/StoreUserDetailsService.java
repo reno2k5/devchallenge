59bd27dda2bf23de2e8b94c5caac06269f3ae23e
Oscar Rivera <reno2k5@gmail.com>
2019-07-21 17:28:24 -0600

    Separación en microservicios

---
+++ service-buy/src/main/java/com/studentsco/store/security/StoreUserDetailsService.java
@@ -0,0 +1,43 @@
+package com.studentsco.store.security;
+
+import com.studentsco.store.dao.security.User;
+import com.studentsco.store.repositories.UsersJPARepository;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StoreUserDetailsService implements UserDetailsService{
+
+    @Autowired
+    private UsersJPARepository userRepo;
+    
+    @Override
+    @Transactional(Transactional.TxType.REQUIRED)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUsername(username);
+        if(user == null){
+            throw new UsernameNotFoundException(username);
+        }
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(), user.getPassword(),
+                getAuthorities(user));
+    } 
+
+    private Collection<? extends GrantedAuthority> getAuthorities(User user) {
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        user.getRoles().forEach((role) -> {
+            authorities.add(new SimpleGrantedAuthority(role.getRole()));
+        });
+        return authorities;
+    }
+
+}
